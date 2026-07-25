033f2afa8f51d6c09a7e83f6af88e2a321cc23fd
Hasan ÖZSOY <hasanzsoy@outlook.com>
2026-07-25 21:24:03 +0300

    Level Sistemi Oluşturuluyor

--- Assets/_project/Scripts/Manager/GameDirector.cs
+++ Assets/_project/Scripts/Manager/GameDirector.cs
@@ -5,6 +5,14 @@ public class GameDirector : MonoBehaviour
     public LevelManager levelManager;
     public Player player;
 
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+            RestartLevel();
+        }
+    }
     private void RestartLevel()
     {
         levelManager.RestartLevelManager();
